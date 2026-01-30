fcf6f574196ba4ada29a2aaf2e0fbe92a72024cd
Adeniyi Buckley <adeniyi.buckley@edu.sait.ca>
2026-01-30 13:05:27 -0700

    Refactor code comments and improve province code checks for consistency

--- Project.py
+++ Project.py
@@ -1,4 +1,20 @@
+'''
+Name: Adeniyi Buckley
+Email: Adeniyi.buckley@edu.sait.ca
+Date: 2025-01-30
+ID#: 000977441
 
+Name: Binaya Lakhe 
+Email: binaya.lakhe.edu.sait.ca
+Date: 2025-01-30
+ID#: 000998156
+
+
+Group 6: Assignment: Programming Basics - Flowchart
+
+
+'''
+#Defininging constants
 bags_per_box = 20
 tea_price_per_bag = 0.45
 coffee_price_per_kg = 18.50
@@ -13,6 +29,7 @@ gst_ab_bc = 0.05
 gst_on = 0.13
 gst_other = 0.15
 
+#Printing Welcome Message and Product Menu
 print("=" * 100)
 print("                           **** Welcome to the Coffee and Tea Wholesale! ****")
 print("=" * 100)
@@ -24,9 +41,9 @@ print("T: Tea Boxes")
 
 product_choice = input("Enter your choice: ")
 product_name = ("")
-product_amount = ("")
-province_code = ("")
-btotal = 0.0 # price before discount
+product_amount = ("") # quantity of product purchased
+province_code = ("")    # For province identification for GST calculation
+btotal = 0.0 # price before discount. To be calculated based on product and quantity
 discount_price = 0.0 # price after discount
 
 # Product Selection and Input
@@ -34,12 +51,12 @@ if product_choice == "c" or product_choice == "C":
     product_name = "Coffee Beans"
     kg_num = float(input("Enter desired number of coffee beans in kilograms: "))
     if kg_num <= 0:
-        print("Invalid quantity: coffee kilograms must be greater than 0")
-        exit()
+        print("Invalid quantity: coffee kilograms must be greater than 0") 
+        exit() # Exit the program if invalid quantity
     province_code = input("Enter your province: ")
     if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code != "Other":
         print("Province choice is invalid")
-        exit()
+        exit() # Exit the program if invalid province code
         
     btotal = kg_num * coffee_price_per_kg
     if kg_num > coffee_discount_theshold_kg:
@@ -53,11 +70,11 @@ elif product_choice == "t" or product_choice =="T":
     box_num = int(input("Enter desired number of tea boxes: "))
     if box_num <= 0:
         print("Invalid quantity: tea boxes must be greater than 0")
-        exit()
+        exit() # Exit the program if invalid quantity
     province_code = input("Enter your province: ")
     if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code != "Other":
         print("Province choice is invalid")
-        exit()
+        exit() # Exit the program if invalid province code
         
     bags = box_num * bags_per_box
     btotal = bags * tea_price_per_bag
@@ -70,7 +87,7 @@ elif product_choice == "t" or product_choice =="T":
 # Calculate GST based on province
 if province_code == "AB" or province_code == "ab" or province_code == "BC" or province_code == "bc":
     gst_rate = gst_ab_bc
-elif province_code == "ON":
+elif province_code == "ON" or province_code == "on":
     gst_rate = gst_on
 elif province_code == "Other":
     gst_rate = gst_other
