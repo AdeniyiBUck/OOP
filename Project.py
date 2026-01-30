18675eadab473347e1b6a5023924dcd4cbd5474b
Adeniyi Buckley <adeniyi.buckley@edu.sait.ca>
2026-01-30 14:46:31 -0700

    Refactor province code validation and improve input handling for product selection

--- Project.py
+++ Project.py
@@ -46,7 +46,11 @@ province_code = ("")    # For province identification for GST calculation
 btotal = 0.0 # price before discount. To be calculated based on product and quantity
 discount_price = 0.0 # price after discount
 
+other_provinces = ["SK", "MB", "QC", "NB", "NS", "PE", "NL", "YT", "NT", "NU", "OTHER", "sk", "mb", "qc", "nb", "ns", "pe", "nl", "yt", "nt", "nu", "other"]
 # Product Selection and Input
+if product_choice != "c" and product_choice != "C" and product_choice != "t" and product_choice !="T":
+    print("Invalid Input has been entered. Please restart the program and enter a valid product choice.")
+    exit() # Exit the program if invalid product choice
 if product_choice == "c" or product_choice == "C":
     product_name = "Coffee Beans"
     kg_num = float(input("Enter desired number of coffee beans in kilograms: "))
@@ -54,8 +58,7 @@ if product_choice == "c" or product_choice == "C":
         print("Invalid quantity: coffee kilograms must be greater than 0") 
         exit() # Exit the program if invalid quantity
     province_code = input("Enter your province: ")
-    if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code != "Other" and province_code != "other":
-        print("Province choice is invalid")
+    if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code not in other_provinces:
         exit() # Exit the program if invalid province code
         
     btotal = kg_num * coffee_price_per_kg
@@ -72,8 +75,7 @@ elif product_choice == "t" or product_choice =="T":
         print("Invalid quantity: tea boxes must be greater than 0")
         exit() # Exit the program if invalid quantity
     province_code = input("Enter your province: ")
-    if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code != "Other" and province_code != "other":
-        print("Province choice is invalid")
+    if province_code != "AB" and province_code != "ab" and province_code != "BC" and province_code != "bc" and province_code != "ON" and province_code != "on" and province_code not in other_provinces:
         exit() # Exit the program if invalid province code
         
     bags = box_num * bags_per_box
@@ -89,7 +91,7 @@ if province_code == "AB" or province_code == "ab" or province_code == "BC" or pr
     gst_rate = gst_ab_bc
 elif province_code == "ON" or province_code == "on":
     gst_rate = gst_on
-elif province_code == "Other" or province_code == "other":
+else:
     gst_rate = gst_other
 
 gst_amount = discount_price * gst_rate
